8c9d4cc4f004864661c2ee99e3388ad50be886ef
Simon Chiang <simon.a.chiang@gmail.com>
2012-07-06 16:19:01 -0600

    Add GeometricSeries

---
+++ lib/series/geometric_series.rb
@@ -0,0 +1,30 @@
+module Series
+  # Describes a geometric series.
+  class GeometricSeries
+    attr_reader :a
+    attr_reader :r
+
+    def initialize(a, r)
+      @a = a
+      @r = r
+    end
+
+    def [](n)
+      a * r ** n
+    end
+
+    def convergent?
+      r.abs < 1
+    end
+
+    def sum(n=nil)
+      if n.nil?
+        convergent? ? (a / (1.0 - r)) : nil
+      else
+        sum = 0
+        n.times {|i| sum += self[i] }
+        sum
+      end
+    end
+  end
+end
