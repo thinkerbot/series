1124a7437a5adf365a6a4494ce73188a56755571
Simon Chiang <simon.a.chiang@gmail.com>
2014-06-30 08:00:03 -0600

    Add -nos options to adjust series driver

--- lib/series/command.rb
+++ lib/series/command.rb
@@ -3,6 +3,9 @@ module Series
     module_function
 
     DEFAULT_OPTIONS = {
+      :n_steps    => 8,
+      :offset     => 0,
+      :step_size  => 1,
     }
 
     module_function
@@ -12,6 +15,20 @@ module Series
 
     def options(overrides = {})
       DEFAULT_OPTIONS.merge(overrides)
+    end
+
+    def driver(options = {})
+      n_steps = options.fetch(:n_steps)
+      start_index = options.fetch(:offset)
+      step_size = options.fetch(:step_size)
+
+      Enumerator.new do |y|
+        current = start_index
+        n_steps.times do
+          y << current
+          current += step_size
+        end
+      end
     end
 
     def loop(stdin = $stdin, stdout = $stdout)
